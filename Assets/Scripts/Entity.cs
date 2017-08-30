df6ab8cb4a7b095ad7873979704a923d09a62d65
Unknown <citrusspoon@gmail.com>
2017-08-30 14:06:19 -0400

    Moved movement code into Entity script

--- Assets/Scripts/Entity.cs
+++ Assets/Scripts/Entity.cs
@@ -4,19 +4,22 @@ using UnityEngine;
 
 public class Entity : MonoBehaviour {
 
-
 	[SerializeField] private bool isPlayer = false;
-
+	public float speed;
+	private Rigidbody rb;
 
 	// Use this for initialization
 	void Start () {
-
-	
-		
+		rb = GetComponent<Rigidbody>();
 	}
-	
+
 	// Update is called once per frame
-	void Update () {
-		
+	void FixedUpdate () {
+		float moveHor = Input.GetAxis("Horizontal");
+		float moveVer = Input.GetAxis("Vertical");
+
+		Vector3 movement = new Vector3(moveHor, moveVer, 0.0f);
+
+		rb.AddForce(movement * speed);
 	}
 }
