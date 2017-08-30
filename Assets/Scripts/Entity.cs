c810a9f5530c87b4bbf1e9c454bc4fbc44ec3c64
Unknown <citrusspoon@gmail.com>
2017-08-30 14:01:32 -0400

    Changed background texture
    Made the background a checkerboard texture so it's easier to see movement

---
+++ Assets/Scripts/Entity.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Entity : MonoBehaviour {
+
+
+	[SerializeField] private bool isPlayer = false;
+
+
+	// Use this for initialization
+	void Start () {
+
+	
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
