c53ed7b58dddf514ed019b0e9ebcd2743ec20d5f
Nathan Grimes <gnathan@uncc.edu>
2017-09-03 16:51:33 -0400

    Simplified the script. Need to lerp player size change

--- Assets/Scripts/Camera_Controller.cs
+++ Assets/Scripts/Camera_Controller.cs
@@ -6,18 +6,22 @@ public class Camera_Controller : MonoBehaviour {
 	public GameObject player;
 	private Vector3 offset, camAdjust;
 	private float oldSize;
+	public Camera thisCamera;
 
 	// Use this for initialization
 	void Start () {
-		offset = transform.position - player.transform.position;
-		oldSize = player.GetComponent<Entity>().size;
-		camAdjust = new Vector3 (0.0f, 0.0f, 0.0f);
+		//offset = transform.position - player.transform.position;
+		//oldSize = player.GetComponent<Entity>().size;
+		//camAdjust = new Vector3 (0.0f, 0.0f, 0.0f);
 
 	}
 
 	// Update is called once per frame
-	void LateUpdate () {
+	void Update () {
 
+		thisCamera.orthographicSize = 4 + player.transform.localScale.x;
+
+		/*
 		if (player) { //prevents errors if player is destroyed
 			if (oldSize < player.GetComponent<Entity>().size) {
 					camAdjust += new Vector3 (0.0f, 0.0f, -5.0f);
@@ -25,5 +29,6 @@ public class Camera_Controller : MonoBehaviour {
 			}
 			transform.position = Vector3.Lerp(transform.position, player.transform.position + offset + camAdjust, .01f);
 		}
+		*/
 	}
 }
