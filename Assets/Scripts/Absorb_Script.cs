2ed2fd291d8962b1ced744ee811751dd55948710
Scott Schreiber <citrusspoon@gmail.com>
2017-09-10 22:42:44 -0400

    Changed glow to code-accessable version
    Still doesn't scale

--- Assets/Scripts/Absorb_Script.cs
+++ Assets/Scripts/Absorb_Script.cs
@@ -2,40 +2,46 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Absorb_Script : MonoBehaviour {
+public class Absorb_Script : MonoBehaviour
+{
 	// Use this for initialization
-	void Start () {
+	void Start ()
+	{
 		
 	}
 
 	//not sure if this is the right method. need to chnage it if we use the 2d one
-	void OnCollisionEnter(Collision col) {
+	void OnCollisionEnter (Collision col)
+	{
 
-		float thisSize = this.gameObject.GetComponent<Entity>().size;
+		float thisSize = this.gameObject.GetComponent<Entity> ().size;
 		float colSize = col.gameObject.GetComponent<Entity> ().size;
 
 		if (thisSize < colSize) {
 			Destroy (this.gameObject);
-			StartCoroutine(this.sizeInc(col.transform));
+			StartCoroutine (this.sizeInc (col.transform));
 		} else if (thisSize > colSize) {
 			Destroy (col.gameObject);
-			StartCoroutine(this.sizeInc(this.transform));
+			StartCoroutine (this.sizeInc (this.transform));
 		} else {
 			//what should we do if they're the same size?
 		}
 	}
 	// Update is called once per frame
-	void Update () {
+	void Update ()
+	{
 
 	}
 
-	IEnumerator sizeInc(Transform source){
+	IEnumerator sizeInc (Transform source)
+	{
 		float deltaScale = 0.5f;
 		float rateOfChange = .1f;
 		Vector3 target = source.localScale + new Vector3 (deltaScale, deltaScale, deltaScale);
 	
 		while (source.localScale.x < target.x) {
 			source.localScale = Vector3.Lerp (source.localScale, target, rateOfChange);
+
 			yield return null;
 		}
 
