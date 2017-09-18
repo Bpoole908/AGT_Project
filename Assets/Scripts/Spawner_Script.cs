e51ca5c430ef7f6b336219fdc16bd3c556ae96b8
Scott Schreiber <citrusspoon@gmail.com>
2017-09-17 22:49:21 -0400

    Made enemy spawner
    Currently just spawns enemies randomly within the bounds of the background. The spawn rate can be specified in the inspector. Later we'll want to make it only spawn outside the camera view

---
+++ Assets/Scripts/Spawner_Script.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner_Script : MonoBehaviour {
+
+
+
+	public float spawnRate;
+	public float startDelay;
+	public GameObject enemyPrefab;
+	GameObject enemyClone;
+	// Use this for initialization
+	void Start () {
+		
+		InvokeRepeating ("spawnEnemy", startDelay, spawnRate);
+		print ("test");
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+
+
+	}
+	void spawnEnemy(){
+
+		enemyClone = Instantiate (enemyPrefab, getRandomV3(), Quaternion.identity) as GameObject;
+
+	}
+
+	Vector3 getRandomV3(){
+		return new Vector3 (Random.Range(-25.0f, 25.0f), Random.Range(-25.0f, 25.0f), 0);
+	}
+}
