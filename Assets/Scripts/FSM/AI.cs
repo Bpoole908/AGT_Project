759d562db9d7ae6174576da13fb6d4f0d1c97755
Bpoole908 <bpoole908@aol.com>
2017-10-01 00:11:22 -0400

    (WIP added FSM)

---
+++ Assets/Scripts/FSM/AI.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using FSM;
+
+public class AI : MonoBehaviour {
+	[HideInInspector] public Transform enemy;
+	public float colliderRadius = 3f;
+
+	public StateMachine<AI> stateMachine { get; set; }
+
+	private void Start()
+	{
+		stateMachine = new StateMachine<AI> (this);
+		stateMachine.ChangeState (State_idle.Instance);
+	}
+	private void Update()
+	{
+		enemy = alertSphere (this.transform.position, colliderRadius);
+
+		if (enemy != null) {
+				stateMachine.ChangeState (State_escape.Instance);
+		} else {
+				stateMachine.ChangeState (State_idle.Instance);
+		}
+	
+	}
+
+
+	/// <summary>
+	/// Gets all colliders within a certain radius of itself.
+	/// </summary>
+	/// <returns>Largest objects transform or null if all other objects are smaller than its scale.</returns>
+	/// <param name="center">Center of alert sphere.</param>
+	/// <param name="radius">Radius or how lard the alert sphere is.</param>
+	public Transform alertSphere(Vector3 center, float radius) {
+		Transform largest = this.transform;
+		Collider[] inAlterSphere = Physics.OverlapSphere (center, radius);
+
+		if (inAlterSphere.Length > 0) {
+			for (int i = 0; i < inAlterSphere.Length; i++) {
+
+				if (inAlterSphere[i].GetComponent<Entity> ().size > largest.GetComponent<Entity> ().size) {
+					largest = inAlterSphere [i].transform;
+				}
+			}
+
+			//If the largest thing is itself return null
+			if (largest == this.transform) {
+				return null;
+			} else {
+				return largest.transform;
+			}
+		}
+		return null;
+	}
+
+}
---
+++ Assets/Scripts/FSM/State_idle.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using FSM;
+
+public class State_idle : State<AI> {
+	private static State_idle instance;
+
+	private State_idle()
+	{
+		if(instance != null)
+			return;
+
+		instance = this;
+	}
+
+	public static State_idle Instance
+	{
+		get 
+		{
+			if (instance == null) {
+				new State_idle ();
+			}
+			return instance;
+		}
+
+	}
+
+	//Do upon entry of state
+	//Meat of the code should go in EnterState for now
+	public override void EnterState(AI owner)
+	{
+
+	}
+
+	//Do before exiting state
+	public override void ExitState(AI owner)
+	{
+
+	}
+
+	//Update to a new state
+	public override void UpdateState(AI owner)
+	{
+
+
+
+	}
+
+
+		
+		
+}
