c1d5d7074bfe96369319903273b4ad1a1d9ad1ff
Scott Schreiber <citrusspoon@gmail.com>
2017-10-05 15:03:18 -0400

    Implemented boost

--- Assets/Scripts/Player_Controller.cs
+++ Assets/Scripts/Player_Controller.cs
@@ -6,13 +6,14 @@ public class Player_Controller : MonoBehaviour
 {
 
 
-	public float minSpeed, maxSpeed;
+	public float minSpeed, maxSpeed, boostSpeed;
 	public float speed;
 	private Rigidbody rb;
 	private Transform tr;
 	[HideInInspector]public AudioSource audio;
 	private float oldSize;
 	public float energy;
+	private bool boosting;
 
 
 	// Use this for initialization
@@ -22,6 +23,7 @@ public class Player_Controller : MonoBehaviour
 		tr = GetComponent<Transform> ();
 		audio = GetComponent<AudioSource> ();
 		oldSize = 0.0f;
+		boosting = false;
 	}
 
 
@@ -32,14 +34,19 @@ public class Player_Controller : MonoBehaviour
 
 		Vector3 movement = new Vector3 (moveHor, moveVer, 0.0f);
 
-		//need to figure out better algorithm here. this one seems a bit harsh
-		speed = (70 - GetComponent<Entity> ().size);
-		//speed = 100/(GetComponent<Entity> ().size);
 
-		if (speed > maxSpeed)
-			speed = maxSpeed;
-		else if (speed < minSpeed)
-			speed = minSpeed;
+
+
+		if (!boosting) {
+
+			speed = (70 - GetComponent<Entity> ().size);
+			if (speed > maxSpeed)
+				speed = maxSpeed;
+			else if (speed < minSpeed)
+				speed = minSpeed;
+		} else {
+			speed = (70 - GetComponent<Entity> ().size) + boostSpeed;
+		}
 
 		tr.Translate (movement * speed / 400); 
 	}
@@ -47,11 +54,17 @@ public class Player_Controller : MonoBehaviour
 	void Update ()
 	{
 
-		if (Input.GetKey (KeyCode.Space)) {
+		if (Input.GetKeyDown (KeyCode.Space)) {
 
-
+			print("boosting");
+			boosting = true;
 		}
 
+		if (Input.GetKeyUp (KeyCode.Space)) {
+
+			print("stop boosting");
+			boosting = false;
+		}
 			
 
 	}
