7d1dd5857718ccb9ffc59e00096fc2f30bec5b1e
Unknown <citrusspoon@gmail.com>
2017-11-13 14:14:37 -0500

    Fixed boosting code

--- Assets/Scripts/Player_Controller.cs
+++ Assets/Scripts/Player_Controller.cs
@@ -62,38 +62,6 @@ public class Player_Controller : MonoBehaviour
 
 		Vector3 movement = new Vector3 (moveHor, moveVer, 0.0f);
 
-
-
-
-		if (!boosting) {
-			Game_Controller.instance.ChangeEnergyBarColor (energyBarColor);
-			speed = (70 - GetComponent<Entity> ().size);
-			if (speed > maxSpeed)
-				speed = maxSpeed;
-			else if (speed < minSpeed)
-				speed = minSpeed;
-			updateEnergy (energyLostPerSec / FRAME_RATE); //Read this as energyLost per second, system is set to 60fps, do not alter the FRAME_RATE value. Adjust energyLostPerSec in the inspector.
-
-		} else {
-			speed = (70 - GetComponent<Entity> ().size) + boostSpeed;
-			Game_Controller.instance.ChangeEnergyBarColor (energyBarBoostColor);
-			updateEnergy (energyLostPerSec*boostEnergyMultiplier / FRAME_RATE); //Read this as energyLost per second, system is set to 60fps, do not alter the FRAME_RATE value. Adjust energyLostPerSec in the inspector.
-
-		}
-		//print (tr.position.x);
-
-		tr.Translate (movement * speed / 400); 
-		float boundsFactor = Game_Controller.instance.planeSize.x*10/2f*0.7f;
-		if (tr.position.x < -1*boundsFactor || tr.position.x > boundsFactor || tr.position.y < -1*boundsFactor || tr.position.y > boundsFactor)
-			tr.Translate(-1*movement * speed / 400);
-
-
-
-	}
-
-	void Update ()
-	{
-
 		if (energy < 1)
 			Game_Controller.instance.GameOver ();
 
@@ -118,6 +86,41 @@ public class Player_Controller : MonoBehaviour
 			ParticleSystem.EmissionModule emission = trail.emission;
 			emission.rateOverTime = 50;
 		}
+
+
+
+
+
+			Game_Controller.instance.ChangeEnergyBarColor (energyBarColor);
+			speed = (70 - GetComponent<Entity> ().size);
+			if (speed > maxSpeed)
+				speed = maxSpeed;
+			else if (speed < minSpeed)
+				speed = minSpeed;
+			updateEnergy (energyLostPerSec / FRAME_RATE); //Read this as energyLost per second, system is set to 60fps, do not alter the FRAME_RATE value. Adjust energyLostPerSec in the inspector.
+
+
+		if(boosting){
+			speed += boostSpeed;
+			Game_Controller.instance.ChangeEnergyBarColor (energyBarBoostColor);
+			updateEnergy (energyLostPerSec*boostEnergyMultiplier / FRAME_RATE); //Read this as energyLost per second, system is set to 60fps, do not alter the FRAME_RATE value. Adjust energyLostPerSec in the inspector.
+		}
+
+		//print (tr.position.x);
+
+		tr.Translate (movement * speed / 400); 
+		float boundsFactor = Game_Controller.instance.planeSize.x*10/2f*0.7f;
+		if (tr.position.x < -1*boundsFactor || tr.position.x > boundsFactor || tr.position.y < -1*boundsFactor || tr.position.y > boundsFactor)
+			tr.Translate(-1*movement * speed / 400);
+
+
+
+	}
+
+	void Update ()
+	{
+
+
 			
 
 
