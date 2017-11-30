3532e30319040b1263aafb062f5cfc35d3ff8613
Nathan Grimes <gnathan@uncc.edu>
2017-11-30 13:54:05 -0500

    Added functionality to set a minimum number of smaller enemies

--- Assets/Scripts/Spawner_Script.cs
+++ Assets/Scripts/Spawner_Script.cs
@@ -15,6 +15,7 @@ public class Spawner_Script : MonoBehaviour {
 	GameObject trailClone;
 	public GameObject player;
 	private GameObject trail;
+	public int minSmallerEnemies = 0;
 
 	void Start () {
 		
@@ -32,18 +33,26 @@ public class Spawner_Script : MonoBehaviour {
 
 	}
 	void spawnEnemy(){
-		float scale;
+		float scale = 0f;
 		if (enemyList.Count < maxEnemies && !Game_Controller.instance.mainMenuActive) {
 			enemyClone = Instantiate (enemyPrefab, getRandomV3 (), Quaternion.identity) as GameObject;
-			if (GetIsSmallerEnemyInList () == false) {
+			if (GetIsEnoughSmallerEnemyInList () == false) {
 				scale = player.transform.localScale.x * .75f;
 				//Debug.Log (scale);
 			} else if (GetIsLargerEnemyInList () == false) {
-				scale = player.transform.localScale.x * 1.25f;
+				scale = player.transform.localScale.x * 3f;
 				//Debug.Log (scale);
 			}
 			else{
-				scale = Random.Range (0.5f, 6.0f);
+				bool appropriateScale = false;
+				while (appropriateScale == false) {
+					scale = Random.Range (0.5f, 6.0f);
+					//if the enemy is too close to the player's size, get new scale value. Size is appropriate if the scale is smaller than the player or larger than player's scale + 1.
+					if (scale < player.transform.localScale.x || scale > player.transform.localScale.x + 0.75f) {
+						appropriateScale = true;
+					} else
+						appropriateScale = false;
+				}
 			}
 			enemyClone.transform.localScale = new Vector3 (scale, scale, scale);
 			trail = enemyClone.gameObject.transform.GetChild (0).gameObject;
@@ -89,12 +98,17 @@ public class Spawner_Script : MonoBehaviour {
 		return rv;
 	}
 
-	bool GetIsSmallerEnemyInList(){
+	bool GetIsEnoughSmallerEnemyInList(){
 		bool result = false;
+		int count = 0;
 		//search the list, if the player is larger than any enemyClone, then there is a smaller enemy in the list.
 		foreach(GameObject enemyClone in enemyList){
-			if (player.transform.localScale.x > enemyClone.transform.localScale.x)
-				result = true;
+			if (player.transform.localScale.x > enemyClone.transform.localScale.x){
+				count++;
+				if(count >= minSmallerEnemies){
+					result = true;
+				}
+			}
 		}
 		return result;
 	}
